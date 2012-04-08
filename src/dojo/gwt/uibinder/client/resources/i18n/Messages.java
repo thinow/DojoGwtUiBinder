4c59350e39d53084fb86b125e4db00fb89882164
Thierry Nowak <thinow@gmail.com>
2012-04-08 18:54:38 +0200

    Création de la ressource "Messages".

---
+++ src/dojo/gwt/uibinder/client/resources/i18n/Messages.java
@@ -0,0 +1,21 @@
+package dojo.gwt.uibinder.client.resources.i18n;
+
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+
+	String rateTitle();
+
+	String descriptionTitle();
+
+	String detailsTitle();
+
+	String commentsTitle();
+
+	String color();
+
+	String alcohol();
+
+	String type();
+
+	String brewerie();
+
+}
